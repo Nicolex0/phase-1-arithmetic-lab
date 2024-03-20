d139270fd987ca6063eac9b249550d64bae1dbdc
Nicole Apono <napononicole@gmail.com>
2024-03-20 07:33:49 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 3) + 1;
+console.log(random);
+
+let num3 = 20;
+let num4 = 8;
+let mod = (num3 % num4 === 4) ? 4 : (num4 - (num3 % num4));
+console.log(mod);
+
+let numbers = [7, 14, 17, 20, 4];
+let max = Math.max(7, 14, 17, 20, 4);
+console.log(max);
